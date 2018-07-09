2d64e94382eb5c24d4a5c2109a89b38910be262a
lhan <never.ending.dr@gmail.com>
2018-07-10 06:59:30 +0900

    fix single body

--- process.py
+++ process.py
@@ -1,18 +1,22 @@
 from email.message import EmailMessage
 from email.parser import BytesParser, Parser
 from email.policy import default
+import json
 
 def to_json(data):
     msg = Parser(policy=default).parsestr(data)
-    print(vars(msg))
+    ans = {k:msg[k] for k in msg.keys()}
+    ans['body'] = {}
     if msg.is_multipart():
-        for payload in msg.get_payload():
-            print(payload.get_payload())
+        for i, payload in enumerate(msg.get_payload()):
+            ans['body'][i] = payload.get_payload()
     else:
-        print(msg.get_payload())
+        ans['body'][0] = msg.get_payload()
+    return json.dumps(ans, indent=4)
 
 if __name__ == "__main__":
     path = input()
     with open(path, 'r', encoding='iso2022_jp') as f:
         data = f.read()
-    to_json(data)
+    ans = to_json(data)
+    print(ans)
